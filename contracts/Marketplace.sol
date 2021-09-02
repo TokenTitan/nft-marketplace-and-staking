256c0599c8dbe73c38098ac64f37c02de64e2f90
Rishabh Sharma <invinciblerishu@gmail.com>
2021-09-02 10:51:26 +0530

    Feature: updated struct, datastructures and added check balance method

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,9 +3,11 @@ pragma solidity 0.8.0;
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
 contract Marketplace is AccessControlUpgradeable {
     uint256 counter;
+    IERC20Upgradeable weth;
 
     enum TokenTypes {
         ERC1155,
@@ -13,7 +15,7 @@ contract Marketplace is AccessControlUpgradeable {
     }
 
     mapping(uint256 => Commodity) public commoditiesForSale;
-    mapping(address => mapping(uint256 => uint256)) internal stock;
+    mapping(address => mapping(uint256 => uint256)) internal saleIdByCommodity;
 
     // keccak256("DEFAULT_ADMIN_ROLE");
     bytes32 internal constant ADMIN_ROLE =
@@ -25,6 +27,7 @@ contract Marketplace is AccessControlUpgradeable {
         address owner;
         address assetContract;
         uint256 id;
+        uint256 amount;
         address acceptedERC20;
         uint256 listedPrice;
         bool listedForSale;
@@ -33,8 +36,9 @@ contract Marketplace is AccessControlUpgradeable {
     // Events
     event ItemListedForSale(uint256 _saleId);
 
-    function initialize() external initializer {
+    function initialize(IERC20Upgradeable _weth) external initializer {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        weth = _weth;
     }
 
     function listForSale(
@@ -58,13 +62,14 @@ contract Marketplace is AccessControlUpgradeable {
             owner: msg.sender,
             assetContract: _erc1155,
             id: _id,
+            amount: _amount,
             acceptedERC20: _acceptedERC20,
             listedPrice: _price,
             listedForSale: true
         });
-        commoditiesForSale[counter] = commodity;
-        stock[_erc1155][_id] = _amount;
-        saleId = counter;
+        saleId = counter++;
+        commoditiesForSale[saleId] = commodity;
+        saleIdByCommodity[_erc1155][_id] = saleId;
         emit ItemListedForSale(counter);
     }
 
@@ -82,29 +87,61 @@ contract Marketplace is AccessControlUpgradeable {
             "Marketplace: Permission not granted to manage asset"
         );
 
-        counter++;
         Commodity memory commodity = Commodity({
             commodityType: TokenTypes.ERC721,
             owner: msg.sender,
             assetContract: _erc721,
             id: _id,
+            amount: 1,
             acceptedERC20: _acceptedERC20,
             listedPrice: _price,
             listedForSale: true
         });
-        commoditiesForSale[counter] = commodity;
-        stock[_erc721][_id] = 1;
-        saleId = counter;
+        saleId = counter++;
+        commoditiesForSale[saleId] = commodity;
+        saleIdByCommodity[_erc721][_id] = saleId;
         emit ItemListedForSale(counter);
     }
 
+    function buy(uint256 _saleId) external {
+        Commodity memory commodity = commoditiesForSale[_saleId];
+        IERC20Upgradeable acceptedERC20 = (commodity.acceptedERC20 ==
+            address(0))
+            ? weth
+            : IERC20Upgradeable(commodity.acceptedERC20);
+
+        require(
+            hasSufficientBalance(acceptedERC20, commodity.listedPrice),
+            "Marketplace: Insufficient Balance"
+        );
+
+        // TODO: Transfer commodity to buyer and update contract state data
+    }
+
+    // function buyByCommodity(address _erc721, uint256 _id) {}
+    // function buyByCommodity(address _erc1155, uint256 _id, uint256 _amount) {}
+
     // Getters
 
-    function checkAvailability(address _token, uint256 _id)
+    function listedForSale(address _token, uint256 _id)
         external
         view
-        returns (uint256)
+        returns (uint256 saleId)
     {
-        return stock[_token][_id];
+        return saleIdByCommodity[_token][_id];
+    }
+
+    function hasSufficientBalance(
+        IERC20Upgradeable _acceptedERC20,
+        uint256 _amount
+    ) internal view returns (bool balanceCheck) {
+        balanceCheck = _acceptedERC20.balanceOf(msg.sender) > _amount;
+        if (
+            !balanceCheck &&
+            _acceptedERC20 == weth &&
+            msg.sender.balance > _amount
+        ) {
+            balanceCheck = true;
+        }
     }
 }
