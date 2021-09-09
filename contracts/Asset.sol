0bd4cdd1a798a5fbd7ac20ab1d4a29b54ad4b67a
Rishabh Sharma <invinciblerishu@gmail.com>
2021-09-09 11:52:56 +0530

    Feature: added public scope to counter in asset contract

--- contracts/Asset.sol
+++ contracts/Asset.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 contract Asset is ERC1155Upgradeable, AccessControlUpgradeable {
-    uint256 counter;
+    uint256 public counter;
 
     // keccak256("DEFAULT_ADMIN_ROLE");
     bytes32 internal constant ADMIN_ROLE =
